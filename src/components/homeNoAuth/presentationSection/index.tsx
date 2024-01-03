01e02f3e76396770a40c089c150f6e83fa2e42db
Bruno Luiz <you@example.comthenrybruno@hotmail.com.br>
2024-01-03 12:38:28 -0300

    Criação dos elementos da Seção de Apresentação e sua responsividade

---
+++ src/components/homeNoAuth/presentationSection/index.tsx
@@ -0,0 +1,32 @@
+import { Button, Col, Container, Row } from 'reactstrap'
+import styles from './styles.module.scss'
+import Link from 'next/link'
+
+const PresentationSection = () => {
+    return (
+        <>
+            <Container className='py-4'>
+                <Row>
+                    <Col md className='d-flex flex-column justify-content-center align-items-start'>
+                        <p className={styles.subtitle}>ACESSO ILIMITADO</p>
+                        <p className={styles.title}>Tenha acesso aos melhores <br />tutoriais de Programação.</p>
+                        <p className={styles.description}>Estude de onde estiver, a qualquer momento, e continue <br />evoluindo como programador.</p>
+                        <Link href='/register'>
+                            <Button outline className={styles.btnCta}>ACESSE AGORA<img src="/buttonPlay.svg" alt="Acesse agora/Play" className={styles.btnImg}/></Button>
+                        </Link>
+                    </Col>
+                    <Col md>
+                        <img src="/homeNoAuth/imgPresentation.png" alt="img Presentation" className={styles.imgPresentation}/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className='d-flex justify-content-center pt-5'>
+                        <img src="/homeNoAuth/iconArrowDown.svg" alt="arrowDown" className={styles.arrowDown}/>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    )
+}
+
+export default PresentationSection
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,22 @@
 import Head from 'next/head'
 import styles from '@/styles/HomeNoAuth.module.scss'
 import HeaderNoAuth from '@/components/homeNoAuth/headerNoAuth'
+import PresentationSection from '@/components/homeNoAuth/presentationSection'
 
 const HomeNoAuth = () => {
-  return(
+  return (
     <>
       <Head>
         <title>OneBitFlix</title>
         <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
-        <meta property='og:title' content='Onebitflix' key='title'/>
-        <meta name='description' content='Tenha acesso aos melhores conteúdos de programação de uma forma simples e fácil!'/>
+        <meta property='og:title' content='Onebitflix' key='title' />
+        <meta name='description' content='Tenha acesso aos melhores conteúdos de programação de uma forma simples e fácil!' />
       </Head>
       <main>
-        <HeaderNoAuth></HeaderNoAuth>
+        <div className={styles.sectionBackground}>
+          <HeaderNoAuth />
+          <PresentationSection />
+        </div>
       </main>
     </>
   )
