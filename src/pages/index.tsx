d90bc6a825dfaa9d6a826b26e39bb763bf9fdcdf
Bruno Luiz <you@example.comthenrybruno@hotmail.com.br>
2024-01-05 12:33:58 -0300

    Criação da seção dos cards da tela homeNoAuth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import styles from '@/styles/HomeNoAuth.module.scss'
 import HeaderNoAuth from '@/components/homeNoAuth/headerNoAuth'
 import PresentationSection from '@/components/homeNoAuth/presentationSection'
+import CardsSections from '@/components/homeNoAuth/cardsSection'
 
 const HomeNoAuth = () => {
   return (
@@ -17,6 +18,7 @@ const HomeNoAuth = () => {
           <HeaderNoAuth />
           <PresentationSection />
         </div>
+        <CardsSections />
       </main>
     </>
   )
