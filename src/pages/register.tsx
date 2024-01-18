3ad423decc9503bfc86812617825929dd0fb2459
Bruno Luiz <you@example.comthenrybruno@hotmail.com.br>
2024-01-18 13:10:37 -0300

    Criação da tela de registri e sua responsividade

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,6 +1,8 @@
+import Footer from '@/components/common/footer'
 import HeaderGeneric from '@/components/common/headerGeneric'
 import styles from '@/styles/registerLogin.module.scss'
 import Head from 'next/head'
+import { Button, Container, Form, FormGroup, Input, Label } from 'reactstrap'
 
 const Register = () => {
     return (
@@ -8,9 +10,46 @@ const Register = () => {
             <Head>
                 <title>OneBitFlix - Registro</title>
                 <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+                <script src="https://jsuites.net/v4/jsuites.js"></script>
             </Head>
-            <main>
+            <main className={styles.main}>
                 <HeaderGeneric logoUrl='/' btnUrl='/login' btnContent='Quero fazer login!'/>
+                <Container className='py-5'>
+                    <p className={styles.formTitle}>Bem vindo(a) ao OneBitFlix!</p>
+                    <Form className={styles.form}>
+                        <p className='text-center'><strong>Faça a sua conta!</strong></p>
+                        <FormGroup>
+                            <Label for='firstName' className={styles.label}>NOME</Label>
+                            <Input id='firstName' name='firstName' type='text' placeholder='Qual é o seu nome?' required maxLength={20} className={styles.inputName}/>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for='lasttName' className={styles.label}>SOBRENOME</Label>
+                            <Input id='lasttName' name='lasttName' type='text' placeholder='Qual é o seu sobrenome?' required maxLength={20} className={styles.inputName}/>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for='phone' className={styles.label}>WHATSAPP</Label>
+                            <Input id='phone' name='phone' type='tel' placeholder='(xx) 9xxxx-xxxx' data-mask='[-]+55 (00) 00000-0000' required className={styles.input}/>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for='email' className={styles.label}>E-MAIL</Label>
+                            <Input id='email' name='email' type='email' placeholder='Informe seu e-mail.' required className={styles.input}/>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for='birth' className={styles.label}>DATA DE NASCIMENTO</Label>
+                            <Input id='birth' name='birth' type='date' min='1930-01-01' max='2013-12-31' required className={styles.input}/>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for='password' className={styles.label}>SENHA</Label>
+                            <Input id='password' name='password' type='password' placeholder='Digite a sua senha: (Min: 6 | Max: 20)' required minLength={6} maxLength={20} className={styles.input}/>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for='password' className={styles.label}>CONFIRME A SUA SENHA</Label>
+                            <Input id='password' name='password' type='password' placeholder='Confirme a sua senha' required minLength={6} maxLength={20} className={styles.input}/>
+                        </FormGroup>
+                        <Button type='submit' outline className={styles.formBtn}>CADASTRAR</Button>
+                    </Form>
+                </Container>
+                <Footer />
             </main>
         </>
     )
